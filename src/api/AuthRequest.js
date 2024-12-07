d3b65f242215db3d7fe420f252b241123ecb3a79
DxnielHz <carlosdanielhv23@gmail.com>
2024-12-06 23:49:30 -0500

    Delete src directory

--- src/api/AuthRequest.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-const api = 'http://localhost:3000/api'
-
-export const registerApi = user => axios.post(`${api}/auth/register`, user)
-export const loginApi = user => axios.post(`${api}/auth/login`, user)
